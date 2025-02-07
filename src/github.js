bd4d094fc6e1d0da057077a56f37202851bb29b9
tyl <tianyl1984@qq.com>
2025-02-07 17:18:05 +0800

    delete log

--- src/github.js
+++ src/github.js
@@ -7,22 +7,15 @@ async function handle(req, env, ctx) {
 	const redirectUri = `https://${url.host}/github/auth`;
 	// 获取accesstoken
 	const tokenJson = await _getAccessToken(env, code, redirectUri);
-	console.log(tokenJson);
 	// 获取登录用户信息
-	console.log(tokenJson.access_token);
 	const userinfo = await _getUserInfo(env, tokenJson.access_token);
-	console.log(userinfo);
 	// 校验用户 && 保存登录信息
 	await _checkLoginUser(env, userinfo);
-	console.log('11111');
 	const token = util.genUUID();
-	console.log(token);
 	await _saveToken(env, token, userinfo);
-	console.log('222222');
 	// 重定向回client
 	let jumpUrl = `https://${url.host}/`;
 	const cb = await _getCallback(env, state);
-	console.log(cb);
 	if (cb == null) {
 		console.log('没有回调，返回首页');
 	} else {
@@ -48,14 +41,13 @@ async function _saveToken(env, token, userinfo) {
 }
 
 async function _checkLoginUser(env, userinfo) {
-	// const users = await env.authdata.get('legal_user');
-	// if (!users.split(',').includes(userinfo.)) {
-	// 	throw new Error('callback error!');
-	// }
+	const users = await env.authdata.get('legal_user');
+	if (!users.split(',').includes(userinfo.login)) {
+		throw new Error('Unauthorized login!');
+	}
 }
 
 async function _getUserInfo(env, accessToken) {
-	console.log(accessToken);
 	const resp = await fetch('https://api.github.com/user', {
 		method: 'GET',
 		headers: {
@@ -65,9 +57,6 @@ async function _getUserInfo(env, accessToken) {
 		}
 	});
 	return resp.json();
-	// const text = await resp.text();
-	// console.log(text);
-	// return JSON.parse(text);
 }
 
 async function _getAccessToken(env, code, redirectUri) {
